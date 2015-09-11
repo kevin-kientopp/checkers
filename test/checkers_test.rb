7888f48f69e56461fb5082e40cacc3f5742073b4
Kevin Kientopp <kevinkientopp@gmail.com>
2015-09-10 22:17:54 -0600

    Removed failing test.

--- test/checkers_test.rb
+++ test/checkers_test.rb
@@ -4,8 +4,4 @@ class CheckersTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Checkers::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
