44f0ca2f78ba7469fe15e33b408a481d14df9cba
Kevin Kientopp <kevinkientopp@gmail.com>
2015-09-12 21:28:39 -0600

    Can click a checker and move it around.

--- lib/checkers/window.rb
+++ lib/checkers/window.rb
@@ -26,6 +26,18 @@ class Window < Gosu::Window
       @game.players[1].checkers.collect { |c| CheckerView.new(c, @blue_checker, @position_calculator.calc_x_for_col(c.col), @position_calculator.calc_y_for_row(c.row)) }
   end
 
+  def button_down(id)
+    if id == Gosu::MsLeft
+      selected_checker_views = @checker_views.select { |c_v| mouse_x > c_v.x && mouse_x < c_v.x + 60 && mouse_y > c_v.y && mouse_y < c_v.y + 60 }
+      selected_checker_views.each { |c_v| c_v.selected, c_v.mouse_x_offset, c_v.mouse_y_offset = true, mouse_x - c_v.x, mouse_y - c_v.y }
+    end
+  end
+
+  def update
+    selected_checker_views = @checker_views.select { |c_v| c_v.selected? }
+    selected_checker_views.each { |c_v| c_v.x, c_v.y = mouse_x - c_v.mouse_x_offset, mouse_y - c_v.mouse_y_offset }
+  end
+
   def draw
     @board_drawer.draw
 
