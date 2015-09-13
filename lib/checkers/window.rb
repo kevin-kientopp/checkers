4dcfb68d25073fd00ef558a994664608e4ecb19b
Kevin Kientopp <kevinkientopp@gmail.com>
2015-09-12 20:59:04 -0600

    Added checker_view.

--- lib/checkers/window.rb
+++ lib/checkers/window.rb
@@ -2,6 +2,7 @@ require 'gosu'
 require_relative 'board_drawer'
 require_relative 'game'
 require_relative 'position_calculator'
+require_relative 'checker_view'
 
 class Window < Gosu::Window
 
@@ -20,13 +21,15 @@ class Window < Gosu::Window
     @blue_checker = Gosu::Image.new(self, 'media/blue_checker.png', true)
 
     @position_calculator = PositionCalculator.new
+
+    @checker_views = @game.players[0].checkers.collect { |c| CheckerView.new(c, @red_checker, @position_calculator.calc_x_for_col(c.col), @position_calculator.calc_y_for_row(c.row)) } +
+      @game.players[1].checkers.collect { |c| CheckerView.new(c, @blue_checker, @position_calculator.calc_x_for_col(c.col), @position_calculator.calc_y_for_row(c.row)) }
   end
 
   def draw
     @board_drawer.draw
 
-    @game.players[0].checkers.each {|c| @red_checker.draw(@position_calculator.calc_x_for_col(c.col), @position_calculator.calc_y_for_row(c.row), 1)}
-    @game.players[1].checkers.each {|c| @blue_checker.draw(@position_calculator.calc_x_for_col(c.col), @position_calculator.calc_y_for_row(c.row), 1)}
+    @checker_views.each { |c_d| c_d.draw }
   end
 
   def needs_cursor?
