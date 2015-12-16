479d590f52573e5e12499cd063d2558445866e7a
Kevin Kientopp <kevinkientopp@gmail.com>
2015-12-16 01:57:59 -0700

    Checker cannot move to col less than 0

--- test/checker_test.rb
+++ test/checker_test.rb
@@ -15,4 +15,11 @@ class CheckerTest < Minitest::Test
     assert_equal 0, checker.row
     assert_equal 0, checker.col
   end
+
+  def test_checker_cannot_move_to_col_less_than_0
+    checker = Checker.new(0, 0, :up)
+    refute checker.move(1, -1)
+    assert_equal 0, checker.row
+    assert_equal 0, checker.col
+  end
 end
