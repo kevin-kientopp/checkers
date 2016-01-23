f33bba1298592828c1c384fff4d5d86e26656b59
Kevin Kientopp <kevinkientopp@gmail.com>
2016-01-23 04:06:44 -0700

    Removed row and col from Checker

--- lib/checkers/checker.rb
+++ lib/checkers/checker.rb
@@ -1,46 +1,11 @@
 class Checker
-  attr_reader :row, :col, :dir, :picked_up
-  alias_method :picked_up?, :picked_up
+  attr_reader :dir
 
-  def initialize(row, col, dir, image = nil)
-    @row, @col, @dir, @image = row, col, dir, image
-  end
-
-  def move(row, col, other_checkers)
-    return false if row < 0 or row > 7 or col < 0 or col > 7
-
-    eligible_spaces = []
-    eligible_spaces << [@row + 1 * dir_multiplier, @col - 1] if space_empty?(@row + 1 * dir_multiplier, @col - 1, other_checkers)
-    eligible_spaces << [@row + 1 * dir_multiplier, @col + 1] if space_empty?(@row + 1 * dir_multiplier, @col + 1, other_checkers)
-    eligible_spaces << [@row + 2 * dir_multiplier, @col - 2] if space_empty?(@row + 2 * dir_multiplier, @col - 2, other_checkers) and enemy_checker_in_space?(@row + 1 * dir_multiplier, @col - 1, other_checkers)
-    eligible_spaces << [@row + 2 * dir_multiplier, @col + 2] if space_empty?(@row + 2 * dir_multiplier, @col + 2, other_checkers) and enemy_checker_in_space?(@row + 1 * dir_multiplier, @col + 1, other_checkers)
-
-    return false if !eligible_spaces.include?([row, col])
-
-    @row, @col = row, col
-    true
+  def initialize(dir, image = nil)
+    @dir, @image = dir, image
   end
 
   def to_s
-    "#{@row}, #{@col}, #{@dir}"
-  end
-
-  private
-  def space_empty?(row, col, checkers)
-    checkers.count { |c| c.row == row and c.col == col } == 0
-  end
-
-  def enemy_checker_in_space?(row, col, checkers)
-    checkers.count { |c| c.row == row and c.col == col and c.dir == opposite_dir } > 0
-  end
-
-  def dir_multiplier
-    return -1 if @dir == :down
-    return 1
-  end
-
-  def opposite_dir
-    return :down if @dir == :up
-    return :up
+    "#{@dir}"
   end
 end
--- lib/checkers/game.rb
+++ lib/checkers/game.rb
@@ -5,7 +5,7 @@ class Game
   attr_reader :players, :board
 
   def start
-    @players = [Player.new(:up, create_player_0_checkers), Player.new(:down, create_player_1_checkers)]
+    @players = [Player.new(:up, Array.new(12, Checker.new(:up))), Player.new(:down, Array.new(12, Checker.new(:down)))]
     @board = Array.new(8) { Array.new(8) }
 
     @players[1].checkers[0..3].each_with_index { |e, index| @board[0][1 + index * 2] = e }
@@ -15,20 +15,5 @@ class Game
     @players[0].checkers[0..3].each_with_index { |e, index| @board[5][index * 2] = e }
     @players[0].checkers[4..7].each_with_index { |e, index| @board[6][1+ index * 2] = e }
     @players[0].checkers[8..11].each_with_index { |e, index| @board[7][index * 2] = e }
-  end
-
-  private
-  def create_player_0_checkers
-    row_0_checkers = Array.new(4) {|i| Checker.new(0, 1 + i * 2, :up)}
-    row_1_checkers = Array.new(4) {|i| Checker.new(1, i * 2, :up)}
-    row_2_checkers = Array.new(4) {|i| Checker.new(2, 1 + i * 2, :up)}
-    row_0_checkers + row_1_checkers + row_2_checkers
-  end
-
-  def create_player_1_checkers
-    row_5_checkers = Array.new(4) {|i| Checker.new(5, i * 2, :down)}
-    row_6_checkers = Array.new(4) {|i| Checker.new(6, 1 + i * 2, :down)}
-    row_7_checkers = Array.new(4) {|i| Checker.new(7, i * 2, :down)}
-    row_5_checkers + row_6_checkers + row_7_checkers
   end
 end
