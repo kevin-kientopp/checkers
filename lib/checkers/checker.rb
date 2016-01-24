c468ec19b29d6eef5c2d39d9202294e3a8db9d9f
Kevin Kientopp <kevinkientopp@gmail.com>
2016-01-23 20:30:16 -0700

    Revert "Removed row and col from Checker"
    This reverts commit f33bba1298592828c1c384fff4d5d86e26656b59.

--- lib/checkers/checker.rb
+++ lib/checkers/checker.rb
@@ -1,11 +1,46 @@
 class Checker
-  attr_reader :dir
+  attr_reader :row, :col, :dir, :picked_up
+  alias_method :picked_up?, :picked_up
 
-  def initialize(dir, image = nil)
-    @dir, @image = dir, image
+  def initialize(row, col, dir, image = nil)
+    @row, @col, @dir, @image = row, col, dir, image
+  end
+
+  def move(row, col, other_checkers)
+    return false if row < 0 or row > 7 or col < 0 or col > 7
+
+    eligible_spaces = []
+    eligible_spaces << [@row + 1 * dir_multiplier, @col - 1] if space_empty?(@row + 1 * dir_multiplier, @col - 1, other_checkers)
+    eligible_spaces << [@row + 1 * dir_multiplier, @col + 1] if space_empty?(@row + 1 * dir_multiplier, @col + 1, other_checkers)
+    eligible_spaces << [@row + 2 * dir_multiplier, @col - 2] if space_empty?(@row + 2 * dir_multiplier, @col - 2, other_checkers) and enemy_checker_in_space?(@row + 1 * dir_multiplier, @col - 1, other_checkers)
+    eligible_spaces << [@row + 2 * dir_multiplier, @col + 2] if space_empty?(@row + 2 * dir_multiplier, @col + 2, other_checkers) and enemy_checker_in_space?(@row + 1 * dir_multiplier, @col + 1, other_checkers)
+
+    return false if !eligible_spaces.include?([row, col])
+
+    @row, @col = row, col
+    true
   end
 
   def to_s
-    "#{@dir}"
+    "#{@row}, #{@col}, #{@dir}"
+  end
+
+  private
+  def space_empty?(row, col, checkers)
+    checkers.count { |c| c.row == row and c.col == col } == 0
+  end
+
+  def enemy_checker_in_space?(row, col, checkers)
+    checkers.count { |c| c.row == row and c.col == col and c.dir == opposite_dir } > 0
+  end
+
+  def dir_multiplier
+    return -1 if @dir == :down
+    return 1
+  end
+
+  def opposite_dir
+    return :down if @dir == :up
+    return :up
   end
 end
