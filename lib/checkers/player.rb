8d8f691adf368bda2143dfbe8e69dd23993a47b2
Kevin Kientopp <kevinkientopp@gmail.com>
2016-01-23 20:31:21 -0700

    Revert "Player can move a checker"
    This reverts commit 2ac7226355303592ac096c7fd07b4a01ca8c6e93.

--- lib/checkers/player.rb
+++ lib/checkers/player.rb
@@ -1,18 +1,8 @@
-require_relative 'checker'
-
 class Player
   attr_reader :direction, :checkers
 
   def initialize(direction, checkers)
     @direction = direction #:up or :down
     @checkers = checkers
-  end
-
-  def move(pos, new_pos, board)
-    checker = board[ pos[:row] ] [ pos[:col] ]
-
-    board[ new_pos[:row] ] [ new_pos[:col] ] = checker
-
-    board[ pos[:row] ] [ pos[:col] ] = nil
   end
 end
